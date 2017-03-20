63af179a6df4cf2db6203490b2fa8aafa6f1c0ae
JinHo Do <greatdjh@gmail.com>
2017-03-20 18:38:46 +0900

    make ChatRoom

---
+++ components/ChatRoom/ChatData.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    threadName: 'Ken and Me',
+    senderId: '12345',
+    senderName: 'Ken',
+    text: '진호님 오늘은 로그인은 저번에 한걸로 마무리된걸로 하시고 트렐로에 제가 3 column 뷰 만드는거 드렸는데 그거 시작해주세요 우선',
+    profile: 'https://scontent-hkg3-1.xx.fbcdn.net/v/t1.0-1/p160x160/10945604_804652766271910_3347291383576024235_n.jpg?oh=8b7547f51429511f62c1202fdbf4160d&oe=596044AA',
+    timestamp: Date.now() - 99999
+  },
+  {
+    threadName: 'Ken and Me',
+    senderId: '12345',
+    senderName: 'Ken',
+    text: '왜 씹으세요? 하기 싫으세요?',
+    profile: 'https://scontent-hkg3-1.xx.fbcdn.net/v/t1.0-1/p160x160/10945604_804652766271910_3347291383576024235_n.jpg?oh=8b7547f51429511f62c1202fdbf4160d&oe=596044AA',
+    timestamp: Date.now() - 89999
+  }
+];
