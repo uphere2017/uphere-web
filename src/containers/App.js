0b237005a212eec9e64755c5f74fe7e63474962c
Ken Huh <kenhuh@Kens-MacBook-Pro.local>
2017-03-31 15:29:41 +0900

    Configure jwt token for every api call

--- src/containers/App.js
+++ src/containers/App.js
@@ -30,10 +30,15 @@ let addFriend;
 let dispatchReceiveNewMessage;
 
 const addFriendPartial = (dispatch) => (friendID) => {
-  axios.get(`${API_URL}/users/${friendID}`)
-    .then(({ data }) =>{
-      dispatch(receiveFriendOnline(data));
-    });
+  axios.get(`${API_URL}/users/${friendID}`, {
+    headers: {
+      authorization: `Bearer ${window.sessionStorage.getItem('accessToken')}`
+    }
+  }).then(({ data }) =>{
+    dispatch(receiveFriendOnline(data));
+  }).catch((err) => {
+    console.error(`[Uphere_WEB] Could not add friend: ${err}`);
+  });
 };
 
 const receiveNewMessagePartial = (dispatch) => (message, chat_id) => {
@@ -66,7 +71,7 @@ const fetchFacebookUserData = (dispatch) => {
     window.FB.api('/me', {fields: 'id,name,email,friends,picture'}, ({ id, name, email = null, friends, picture }) => {
       dispatch(receiveFBUserData({ name, email, picture }));
 
-      axios.post(API_URL + '/users', {
+      axios.post(API_URL + '/login', {
         facebook_id: id,
         profile_image_url: picture.data.url,
         email_address: email,
@@ -76,23 +81,28 @@ const fetchFacebookUserData = (dispatch) => {
         .then(({ data }) => {
           dispatch(receiveUserData({ user: data.user }));
 
+          window.sessionStorage.setItem('accessToken', data.accessToken);
+
           socket.emit('LOG_IN', {
             user_uphere_id: data.user.uphere_id
           });
 
-          axios.get(`${API_URL}/users/${data.user.uphere_id}/friend-list`)
-            .then(response => {
-              socket.emit('USER_ONLINE', {
-                user_uphere_id: data.user.uphere_id,
-                friend_list: response.data.map((friend) => friend.uphere_id)
-              });
-              dispatch(receiveFriendList(response.data));
-              chatListRequest(dispatch, data.user, response.data);
-              resolve();
-            })
-            .catch(err => {
-              console.error('[UPHERE_WEB] Could not get friend list', err)
+          axios.get(`${API_URL}/users/${data.user.uphere_id}/friend-list`, {
+            headers: {
+              authorization: `Bearer ${window.sessionStorage.getItem('accessToken')}`
+            }
+          }).then(response => {
+            socket.emit('USER_ONLINE', {
+              user_uphere_id: data.user.uphere_id,
+              friend_list: response.data.map((friend) => friend.uphere_id)
             });
+            dispatch(receiveFriendList(response.data));
+            chatListRequest(dispatch, data.user, response.data);
+            resolve();
+          })
+          .catch(err => {
+            console.error('[UPHERE_WEB] Could not get friend list:', err)
+          });
         })
         .catch(reject);
     });
@@ -100,36 +110,40 @@ const fetchFacebookUserData = (dispatch) => {
 };
 
 const chatListRequest = (dispatch, user, friendList) => {
-  return axios.get(`${API_URL}/users/${user.uphere_id}/chats`)
-              .then(({ data }) => {
-                data.chats.map((chat, i) => {
-                  let userIndex;
-                  let friendIndex;
+  return axios.get(`${API_URL}/users/${user.uphere_id}/chats`, {
+    headers: {
+      authorization: `Bearer ${window.sessionStorage.getItem('accessToken')}`
+    }
+  }).then(({ data }) => {
+    data.chats.map((chat, i) => {
+      let userIndex;
+      let friendIndex;
 
-                  if(chat.participants.includes(user.uphere_id)) {
-                    userIndex = chat.participants.indexOf(user.uphere_id);
-                    chat.participants[userIndex] = user;
-                  }
+      if(chat.participants.includes(user.uphere_id)) {
+        userIndex = chat.participants.indexOf(user.uphere_id);
+        chat.participants[userIndex] = user;
+      }
 
-                  friendIndex = userIndex === 0 ? 1 : 0;
+      friendIndex = userIndex === 0 ? 1 : 0;
 
-                  const friend = friendList.filter((friend) => {
-                    return friend.uphere_id === chat.participants[friendIndex];
-                  })[0];
+      const friend = friendList.filter((friend) => {
+        return friend.uphere_id === chat.participants[friendIndex];
+      })[0];
 
-                  chat.participants[friendIndex] = friend;
+      chat.participants[friendIndex] = friend;
 
-                  return chat;
-                });
+      return chat;
+    });
 
-                if (data.chats.length > 0) {
-                  dispatch(requestChatListSuccess(data.chats));
-                  dispatch(updateCurrentChatroom(data.chats[0]));
-                }
-              })
-              .catch(err => {
-                dispatch(requestChatListFailure(err));
-              });
+    if (data.chats.length > 0) {
+      dispatch(requestChatListSuccess(data.chats));
+      dispatch(updateCurrentChatroom(data.chats[0]));
+    }
+  })
+  .catch(err => {
+    dispatch(requestChatListFailure(err));
+    console.error(`[Uphere_WEB] Could not retrieve chat list: ${err}`);
+  });
 };
 
 const mapStateToProps = (state) => {
@@ -176,14 +190,18 @@ const mapDispatchToProps = (dispatch) => {
       axios.post(API_URL + '/chats', {
         participants: [hostID, friendID],
         messages: []
+      }, {
+        headers: {
+          authorization: `Bearer ${window.sessionStorage.getItem('accessToken')}`
+        }
+      }).then((res) => {
+        dispatch(updateCurrentChatroom(res.data.chat));
+        dispatch(createChatSuccess(res.data.chat));
       })
-        .then((res) => {
-          dispatch(updateCurrentChatroom(res.data.chat));
-          dispatch(createChatSuccess(res.data.chat));
-        })
-        .catch(err => {
-          dispatch(createChatFailure(err));
-        })
+      .catch(err => {
+        dispatch(createChatFailure(err));
+        console.error(`[Uphere_WEB] Could not create new chat: ${err}`);
+      });
     },
 
     onLogin: ({ id }) => {
@@ -200,29 +218,34 @@ const mapDispatchToProps = (dispatch) => {
       axios.post(API_URL + `/chats/${chatroom.uphere_id}`, {
         text: message,
         sender_id: user.uphere_id
-      })
-        .then(({ data }) => {
-          dispatch(createChatMessage({
-            chatroom: chatroom,
-            text_id: data.id,
-            user_id: user.uphere_id,
-            text: message,
-            created_at: data.created_at
-          }));
+      }, {
+        headers: {
+          authorization: `Bearer ${window.sessionStorage.getItem('accessToken')}`
+        }
+      }).then(({ data }) => {
+        dispatch(createChatMessage({
+          chatroom: chatroom,
+          text_id: data.id,
+          user_id: user.uphere_id,
+          text: message,
+          created_at: data.created_at
+        }));
 
-          const receipient_id = chatroom.participants.filter((chatUser) => {
-            return chatUser.uphere_id !== user.uphere_id;
-          })[0].uphere_id;
+        const receipient_id = chatroom.participants.filter((chatUser) => {
+          return chatUser.uphere_id !== user.uphere_id;
+        })[0].uphere_id;
 
-          socket.emit('SEND_NEW_MESSAGE', {
-            sender_id: user.uphere_id,
-            chat_id: chatroom.uphere_id,
-            text: message,
-            created_at: data.created_at,
-            text_id: data.id,
-            receipient_id
-          });
+        socket.emit('SEND_NEW_MESSAGE', {
+          sender_id: user.uphere_id,
+          chat_id: chatroom.uphere_id,
+          text: message,
+          created_at: data.created_at,
+          text_id: data.id,
+          receipient_id
         });
+      }).catch((err) => {
+        console.error(`[Uphere_WEB] Could not create new message: ${err}`);
+      });
     }
   };
 };
